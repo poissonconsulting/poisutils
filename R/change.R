07d74d04bfb3792d2bf18b1b7aca8efb2a45fa63
Joe Thorley <joe@poissonconsulting.ca>
2018-02-07 09:33:34 -0800

    add change functions

---
+++ R/change.R
@@ -0,0 +1,47 @@
+#' n-Fold Change
+#'
+#' @param x A vector of the before values
+#' @param y A vector of the after values
+#'
+#' @return A vector of the n-fold change
+#' @export
+#'
+#' @examples
+#' ps_nfold_change(3, c(3, 1, 9))
+ps_nfold_change <- function(x, y) {
+  x <- y / x
+  ifelse(x >= 1, x, -x^-1)
+}
+
+
+#' Proportional Change
+#'
+#' @param x A vector of the before values
+#' @param y A vector of the after values
+#'
+#' @return A vector of the proportional change
+#' @export
+#'
+#' @examples
+#' ps_prop_change(3, c(3, 1, 9))
+ps_prop_change <- function(x, y) {
+  (y - x) / x
+}
+
+#' Proportional Change to n-Fold Change
+#'
+#' @param x A numeric vector of the proportional change
+#'
+#' @return A numeric vector of the n-fold change
+#' @export
+#'
+#' @examples
+#' ps_nfold_change(3, c(3, 1, 9))
+#' ps_prop_change(3, c(3, 1, 9))
+#' ps_prop2nfold_change(ps_prop_change(3, c(3, 1, 9)))
+ps_prop2nfold_change <- function(x) {
+  check_vector(x, 1)
+  if(!length(x)) return(x)
+  x <- x + 1
+  ifelse(x >= 1, x, -x^-1)
+}
---
+++ tests/testthat/test-change.R
@@ -0,0 +1,8 @@
+context("change")
+
+test_that("change", {
+  expect_equal(ps_nfold_change(3, c(3, 1, 9)), c(1, -3, 3))
+  expect_equal(ps_prop_change(3, c(3, 1, 9)), c(0, -0.66666667, 2))
+  expect_equal(ps_prop2nfold_change(ps_prop_change(3, c(3, 1, 9))),
+                  ps_nfold_change(3, c(3, 1, 9)))
+})

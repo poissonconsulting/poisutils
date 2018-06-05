100b32b24d36831adc12793eec7022f2f1469452
Joe Thorley <joe@poissonconsulting.ca>
2018-06-05 10:45:37 -0700

    add ps_round_preserve_sum

---
+++ R/round-preserve-sum.R
@@ -0,0 +1,22 @@
+#' Round Values while Preserving their Rounded Sum
+#'
+#' @param x A numeric vector of values to round
+#' @param digits A count of the number of decimals for rounding
+#' @source \url{http://biostatmatt.com/archives/2902}
+#' @examples
+#' sum(c(0.33, 0.33, 0.33))
+#' round(c(0.33, 0.33, 0.33), 1)
+#' sum(round(c(0.33, 0.33, 0.33), 1))
+#' ps_round_preserve_sum(c(0.33, 0.33, 0.33), 1)
+#' sum(ps_round_preserve_sum(c(0.33, 0.33, 0.33), 1))
+#' @export
+ps_round_preserve_sum <- function(x, digits = 0L) {
+  digits <- check_count(digits, coerce = TRUE)
+
+  up <- 10^digits
+  x <- x * up
+  y <- floor(x)
+  indices <- utils::tail(order(x - y), round(sum(x)) - sum(y))
+  y[indices] <- y[indices] + 1
+  y / up
+}

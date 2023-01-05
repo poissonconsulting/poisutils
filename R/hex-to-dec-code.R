580327ae387ccb13c7b3a28954996d47fa422832
Ayla Pearson <ayla@poissonconsulting.ca>
2023-01-04 17:11:49 -0800

    adding new function to package - adding `hex_to_dec_code()` function which converts hecidemical codes to decimal codes.

---
+++ R/hex-to-dec-code.R
@@ -0,0 +1,16 @@
+#' Hexadecimal to Decimal PIT Tag Converter
+#'
+#' @param hex A string of the hex code.
+#'
+#' @return An double.
+#' @export
+#'
+#' @examples
+#' hex_to_dec_code("349EA72A50")
+#' hex_to_dec_code("14A5D0BE89")
+hex_to_dec_code <- function(hex) {
+  chk::chk_string(hex)
+  dec <- as.numeric(Rmpfr::mpfr(hex, base=16))
+  dec
+}
+

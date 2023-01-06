3d8de8af13a617ace1db9cb39ccfe669af93495d
Ayla Pearson <ayla@poissonconsulting.ca>
2023-01-05 16:23:22 -0800

    fixing types in tests to match docs

--- tests/testthat/test-hex-to-dec-pit-tag.R
+++ tests/testthat/test-hex-to-dec-pit-tag.R
@@ -7,28 +7,28 @@ test_that("default is to error", {
 
 test_that("default country number is appended to front of conversion", {
   dec_code <- hex_to_dec_pit_tag("349EA72A50")
-  expect_identical(dec_code, 900226000054864)
+  expect_identical(dec_code, "900226000054864")
 })
 
 test_that("output is 15 digits long when dec outputted is 12 digits long", {
   dec_code <- hex_to_dec_pit_tag("349EA72A50")
-  expect_identical(dec_code, 900226000054864)
+  expect_identical(dec_code, "900226000054864")
   expect_length(dec_code, 15L)
 })
 
 test_that("output is 15 digits long when dec outputted is 11 digits long", {
   dec_code <- hex_to_dec_pit_tag("49EA72A50")
-  expect_identical(dec_code, 900019841624656)
+  expect_identical(dec_code, "900019841624656")
   expect_length(dec_code, 15L)
 })
 
 test_that("output is NA when dec outputted more then 12 digits long", {
   dec_code <- hex_to_dec_pit_tag("49EA72A5000")
-  expect_identical(dec_code, NA_real_)
+  expect_identical(dec_code, NA_character_)
 })
 
 test_that("country code changes when passed", {
   dec_code <- hex_to_dec_pit_tag("349EA72A50", country_code = 800)
-  expect_identical(dec_code, 800226000054864)
+  expect_identical(dec_code, "800226000054864")
   expect_length(dec_code, 15L)
 })

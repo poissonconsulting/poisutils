c479850d5340bb0526db9da713e2a6ad7aed1016
Ayla Pearson <ayla@poissonconsulting.ca>
2025-06-09 12:16:29 -0700

    updating docs because of deprecation error when re-building docs

--- R/package.R
+++
@@ -1,9 +0,0 @@
-#' poisutils
-#'
-#' Utility functions for Poisson Consulting's scripts and packages.
-#'
-#' @seealso <https://github.com/poissonconsulting/poisutils>
-#' @name poisutils
-#' @aliases package-poisutils poisutils-package
-#' @docType package
-NULL
